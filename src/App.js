7d6ac38904a07e8a2ab9e024b4fe3796942e1846
Arbnor Raci <raci@aivy.app>
2024-10-28 10:21:53 +0100

    Optimize components export with index.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import Logo from "./components/Logo";
-import MarkdownEditor from "./components/MarkdownEditor";
-import NoteList from "./components/NoteList";
+import { Logo, MarkdownEditor, NoteList } from "./components";
 import { NotesProvider } from "./context/NoteContext";
 
 function App() {
   return (
     <NotesProvider>
       <div className="flex items-center justify-center h-screen w-full bg-gray-100 md:px-2 lg:px-0">
-        <div className="flex flex-row bg-white h-screen md:h-4/6 w-full max-w-5xl shadow-2xl shadow-gray-200 rounded-none md:rounded-xl overflow-hidden">
+        <div className="flex flex-row bg-white h-screen md:h-5/6 w-full max-w-5xl shadow-2xl shadow-gray-200 rounded-none md:rounded-xl overflow-hidden">
           <div className="w-72 bg-white border-r">
             <div className="py-4 border-b min-h-20 flex items-center justify-center">
               <Logo />
---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export { default as CreateNoteButton } from "./CreateNoteButton";
+export { default as DeleteNoteButton } from "./DeleteNoteButton";
+export { default as ListEmptyComponent } from "./ListEmptyComponent";
+export { default as Logo } from "./Logo";
+export { default as MarkdownEditor } from "./MarkdownEditor";
+export { default as NoteList } from "./NoteList";
+export { default as NoteListItem } from "./NoteListItem";
+export { default as Toolbar } from "./Toolbar";
